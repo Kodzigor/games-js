9831225b3f8443a9f84e80462a5c6e292a9dec73
Igor Kodzhebash <igorkodzebash@gmail.com>
2024-10-13 17:34:23 +0300

    feat: handle with current scores for each player

--- pig-game/main.js
+++ pig-game/main.js
@@ -2,19 +2,36 @@
 
 const player1 = document.querySelector('.player--0');
 const player2 = document.querySelector('.player--1');
-let score1 = document.querySelector('.current--1');
-let score2 = document.querySelector('.current--2');
+const current1 = document.querySelector('#current--1');
+const current2 = document.querySelector('#current--2');
+const score1 = document.querySelector('#score1--1');
+const score2 = document.querySelector('#score1--2');
 const buttonNew = document.querySelector('.btn--new');
 const buttonRoll = document.querySelector('.btn--roll');
 const buttonHold = document.querySelector('.btn--hold');
-const dice = document.querySelector('.dice');
+const diceEl = document.querySelector('.dice');
 
 let scores = [0, 0];
+let currentScore = 0;
 let currentPlayer = 1;
+diceEl.classList.add('hidden');
 
 // Rolling the dice
 buttonRoll.addEventListener('click', () => {
-  let random = Math.floor(Math.random() * 6) + 1;
+  let dice = Math.floor(Math.random() * 6) + 1;
 
-  dice.src = `./images/dice-${random}.png`;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `./images/dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    scores[currentPlayer - 1] += dice;
+    document.querySelector(`#current--${currentPlayer}`).textContent = currentScore;
+  } else {
+    document.querySelector(`#current--${currentPlayer}`).textContent = 0;
+    currentPlayer = currentPlayer == 1 ? 2 : 1;
+    currentScore = 0;
+  }
+
+  console.log(scores);
 });
